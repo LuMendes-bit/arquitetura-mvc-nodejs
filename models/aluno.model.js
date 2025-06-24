dc865e6fb649f431442a7d9254e6a2a28a5886ed
Lucas Mendes <lucas.mendes.1234@hotmail.com>
2025-06-23 22:14:25 -0300

    alunos e professores mvc

--- models/aluno.model.js
+++ models/aluno.model.js
@@ -1,9 +1,10 @@
 function listarAlunos(){
     const alunos = [
-        {id:1, nome:"Lucas Mendes", idade:22 ,curso:"Técnico em informatica"},
-        {id:2, nome:"Matheus", idade:29 ,curso:"Técnico em segurança de trabalho"},
-        {id:3, nome:"Maria clara", idade:21 ,curso:"Técnico em emfermagem"}
+        {id:1, nome:"Lucas Mendes", idade:22, curso:"Técnico em informática", RA: "123456"},
+        {id:2, nome:"Matheus", idade:29, curso:"Técnico em segurança de trabalho", RA: "654321"},
+        {id:3, nome:"Maria Clara", idade:21, curso:"Técnico em enfermagem", RA: "112233"}
     ]
+    
     return alunos
 }
 
